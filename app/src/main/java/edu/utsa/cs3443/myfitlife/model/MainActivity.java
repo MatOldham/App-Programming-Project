d2b4edcb9cb6a293bd5ae94446d4de0245e9cc57
Jae Lee <jhlee17b@gmail.com>
2023-04-17 12:15:08 -0500

    add layouts

--- app/src/main/java/edu/utsa/cs3443/myfitlife/model/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/myfitlife/model/MainActivity.java
@@ -1,8 +1,10 @@
-package edu.utsa.cs3443.myfitlife;
+package edu.utsa.cs3443.myfitlife.model;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import edu.utsa.cs3443.myfitlife.R;
 
 public class MainActivity extends AppCompatActivity {
 //hello world
