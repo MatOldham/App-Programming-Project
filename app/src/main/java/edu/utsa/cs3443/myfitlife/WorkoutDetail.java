a38c87847b41fe45297e4b9f5c02e811442263a5
MatOldham <moldham02@gmail.com>
2023-05-01 15:24:06 -0500

    Added images to workoutDetail.

--- app/src/main/java/edu/utsa/cs3443/myfitlife/WorkoutDetail.java
+++ app/src/main/java/edu/utsa/cs3443/myfitlife/WorkoutDetail.java
@@ -3,7 +3,9 @@ package edu.utsa.cs3443.myfitlife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WorkoutDetail extends AppCompatActivity {
@@ -31,6 +33,29 @@ public class WorkoutDetail extends AppCompatActivity {
         TextView title = (TextView)findViewById(R.id.textView3);
         TextView desc = (TextView)findViewById(R.id.textView4);
         TextView numOf = (TextView)findViewById(R.id.textView5);
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+
+        switch(workName){
+
+            case "Burpees":
+                image.setImageResource(R.drawable.burpee);
+                break;
+            case "Situps":
+                image.setImageResource(R.drawable.situp);
+                break;
+            case "Pushups":
+                image.setImageResource(R.drawable.pushup);
+                break;
+            case "Pullups":
+                image.setImageResource(R.drawable.pullup);
+                break;
+            case "Dips":
+                image.setImageResource(R.drawable.dips);
+                break;
+            default:
+                Log.d("imageError","Image for workout not found");
+
+        }
 
         //Sets all of the text correctly
         title.setText(workName);
