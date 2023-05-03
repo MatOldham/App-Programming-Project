134fe91d67ac3b472bbb96fa6ec5d8fd562b1366
Trevor Coyle <trevorcoyle23@gmail.com>
2023-05-03 10:04:16 -0500

    added functionality to buttons in Receipt classes

--- app/src/main/java/edu/utsa/cs3443/myfitlife/Receipt.java
+++ app/src/main/java/edu/utsa/cs3443/myfitlife/Receipt.java
@@ -2,17 +2,52 @@ package edu.utsa.cs3443.myfitlife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 import android.os.Bundle;
+import android.view.View;
 
-import edu.utsa.cs3443.myfitlife.R;
+import edu.utsa.cs3443.myfitlife.model.Workout;
 
 public class Receipt extends AppCompatActivity {
-
+    WorkoutList workoutList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
+
+        Button back = findViewById(R.id.button8);
+        Button exit = findViewById(R.id.button10);
+
+
+
+        TextView burpeesText = findViewById(R.id.textView17);
+        TextView dipsText = findViewById(R.id.textView10);
+        TextView pullupText = findViewById(R.id.textView7);
+        TextView pushupText = findViewById(R.id.textView8);
+        TextView situpText = findViewById(R.id.textView9);
+
+        String workName = getIntent().getStringExtra("workName");
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
+
+        do {
+            //prints out contents
+        } while(workoutList != null);
     }
-
-
 }
--- app/src/main/java/edu/utsa/cs3443/myfitlife/ReceiptController.java
+++ app/src/main/java/edu/utsa/cs3443/myfitlife/ReceiptController.java
@@ -2,6 +2,7 @@ package edu.utsa.cs3443.myfitlife;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import edu.utsa.cs3443.myfitlife.model.Workout;
@@ -13,9 +14,7 @@ public class ReceiptController implements View.OnClickListener{
     }
     @Override
     public void onClick(View view) {        //current activity, started activity
-        //Calculate everything here
+        Intent intent = new Intent(workoutList, Receipt.class);
 
-
-        workoutList.startActivity(new Intent(workoutList, Receipt.class));
     }
 }
